323065d6dced5e5de0042354a2ca6d2ba3dd4b3d
Sacha Greif <sacha357@gmail.com>
2012-04-10 11:03:40 +0900

    changed clicky code

--- wp-content/themes/folyoblog/footer.php
+++ wp-content/themes/folyoblog/footer.php
@@ -34,8 +34,8 @@
 ?>
 <?php if($_SERVER['HTTP_HOST']=="blog.folyo.me"){?>
 	<script src="//static.getclicky.com/js" type="text/javascript"></script>
-	<script type="text/javascript">try{ clicky.init(66362194); }catch(e){}</script>
-	<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66362194ns.gif" /></p></noscript>
+	<script type="text/javascript">try{ clicky.init(66567886); }catch(e){}</script>
+	<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66567886ns.gif" /></p></noscript>
 <?php } ?>
 
 	<div id="fb-root"></div>
