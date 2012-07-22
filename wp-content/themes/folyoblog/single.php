298d43a1e7dbd61b8b4b131b0199d7c024e74e05
Sacha Greif <sacha357@gmail.com>
2012-07-22 17:41:58 +0900

    added add this analytics

--- wp-content/themes/folyoblog/single.php
+++ wp-content/themes/folyoblog/single.php
@@ -85,6 +85,10 @@ get_header();
 							?>
 						</div>
 						<div class="entry-social">
+							<!-- AddThis Button BEGIN -->
+<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;pubid=ra-500bbc997c97bee3"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-500bbc997c97bee3"></script>
+<!-- AddThis Button END -->
 				<!-- 			<div class="twitter">
 								<a href="https://twitter.com/share" class="twitter-share-button" data-via="YoFolyo" >Tweet</a>
 								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
